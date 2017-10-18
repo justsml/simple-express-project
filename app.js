18d8bdfae6a774a6c9ee89d60486540c7ae925ea
Dan Levy <397632+justsml@users.noreply.github.com>
2017-10-18 09:08:24 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const morgan = require('morgan')
+const http        = require('http')
+const express     = require('express')
+const bodyParser  = require('body-parser')
+const morgan      = require('morgan')
+const cors        = require('cors')
+const app         = module.exports = express()
+const server      = http.createServer(app)
+const port        = parseInt(process.env.PORT || 3000)
+
 const books = require('./routes/book')
 
-app = express()
-
-app.use(bodyParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan('dev'))
+app.use(cors({origin: true}))
 
 app.use('/books', books)
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.use(notFound)
+app.use(errorHandler)
 
+server.listen(port)
+  .on('error', console.error.bind(console))
+  .on('listening', console.log.bind(console, 'Listening on ' + port));
+
+
+function notFound(req, res, next) {
+  res.status(404)
+    .send({error: 'Url not found', status: 404, url: req.url})
+}
+
+function errorHandler(err, req, res, next) {
+  console.error('ERROR', err)
+  res.status(500)
+    .send({error: err, url: req.url, status: 500})
+}
